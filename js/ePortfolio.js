046ea9ab8e43fce05285bc17dd7e2ef89519a5a7
Ivan Matejcic <ivanmatejcic1@gmail.com>
2022-05-21 13:48:30 +0200

    Commit 2: Additional styling.

--- js/ePortfolio.js
+++ js/ePortfolio.js
@@ -1,3 +1,5 @@
+/*HAMBURGER*/
+
 function myFunction() {
   var x = document.getElementById("navbar-list");
   
@@ -7,3 +9,33 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+/*SLIDES*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("opis");
+  if (n > slides.length){
+    slideIndex = 1
+  };    
+  if (n < 1){
+    slideIndex = slides.length
+  };
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  };
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000);
+};
+
