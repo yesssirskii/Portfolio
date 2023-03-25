816169f4ddc191358797ecb6d44ab5d9175d967a
IvanMatejcic <ivanmatejcic1@gmail.com>
2023-03-25 21:03:03 +0100

    Portfolio update.

--- js/ePortfolio.js
+++ js/ePortfolio.js
@@ -12,17 +12,10 @@ function myFunction() {
 
 /*SLIDES*/
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+/**
+ * Function which creates and meneges the HTML carousel.
+ * @param {*} n 
+ */
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("opis");
@@ -38,4 +31,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 5000);
 };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/**
+ * Go to the next slide in the carousel.
+ * @param {*} n 
+ */
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/**
+ * Setting the current slide.
+ * @param {*} n 
+ */
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+/**
+ * Open CV PDF in new tab.
+ */
+function openCV() {
+  let link = document.createElement("a");
+  link.href = "Ivan Matejčić_CV.pdf";
+  link.target = "_blank";
+  link.click();
+}
 
